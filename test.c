9df9fb68bf0891f9d86e2bb709748ba900497934
Houxu289747732 <44091585+Houxu289747732@users.noreply.github.com>
2019-10-16 12:32:13 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char enter[1000] = {0};
+	char info[1000];
+	FILE *fp = NULL;
+	printf("Enter the student No. that you want to check:  \n");
+	scanf("%s", enter);
+	if((fp = fopen(enter, "r")) == NULL)
+		printf("Fail");
+	while(fgets(info, sizeof(info), fp))
+		printf("%s", info);
+	fclose(fp);
+	return 0;
+}
+
